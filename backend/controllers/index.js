c58d3ab0e10a682b2cabe106e7a955cf4dcff195
rini1 <rini@chisquarelabs.com>
2025-01-09 06:08:37 +0530

    Accessment

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -2,6 +2,7 @@ const { Client } = require("pg");
 const Sequelize = require("sequelize");
 const db = require("../models");
 const Users = require("../models").users;
+const PatientAccessment = require("../models").PatientAccessment;
 
 const Login = async (req, res) => {
   try {
@@ -25,21 +26,93 @@ const Login = async (req, res) => {
   }
 };
 
-const Register = (req, res) => {
-  console.log("req---", req.body);
-  // if (req.body.email === adminEmail && req.body.password === adminPassword) {
-  //   res.status(200).send({
-  //     message: "Admin register in successfully",
-  //     success: true,
-  //   });
-  // } else {
-  //   res
-  //     .status(500)
-  //     .send({ message: "Username or password is incorrect", success: false });
-  // }
+const Register = async (req, res) => {
+  try {
+    console.log("req.body---", req.body);
+    await Users.create({
+      email: req.body.email,
+      password: req.body.password,
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      dob: req.body.dob,
+      gender: req.body.gender,
+      ethinicity: req.body.ethinicity,
+      role_id: req.body.role_id,
+    });
+
+    res
+      .status(200)
+      .send({ message: "User created successfully", success: true });
+  } catch (err) {
+    console.log("errr---", err);
+    res.status(500).send({ message: "User not created", success: false });
+  }
+};
+
+const Assessment = async (req, res) => {
+    let score;
+  try {
+    const { patient_id, assessments } = req.body;
+
+    for (const sec of assessments) {
+        //If Ans: NO
+        if(sec.answer_id === 0)
+            score=-1;
+        //If Ans: Yes
+        if(sec.answer_id !== 0)
+            score=+1;
+        // Create the Section record for each answer with associated patient_id and question_id
+        await PatientAccessment.create({
+          patient_id,
+          question_id: sec.question_id,
+          answer: sec.answer_id,
+          score,
+        });
+      }
+
+    res.status(200).send({
+      message: "Accessment submitted successfully",
+      success: true,
+    });
+  } catch (err) {
+    console.log("errr---", err);
+    res.status(500).send({ message: "User not created", success: false });
+  }
+};
+
+const Summery = async (req, res) => {
+  try {
+    const { patient_id, assessments } = req.body;
+
+    for (const sec of assessments) {
+        //If Ans: NO
+        if(sec.answer_id === 0)
+            score=-1;
+        //If Ans: Yes
+        if(sec.answer_id !== 0)
+            score=+1;
+        // Create the Section record for each answer with associated patient_id and question_id
+        await PatientAccessment.create({
+          patient_id,
+          question_id: sec.question_id,
+          answer: sec.answer_id,
+          score,
+        });
+      }
+
+    res.status(200).send({
+      message: "Accessment submitted successfully",
+      success: true,
+    });
+  } catch (err) {
+    console.log("errr---", err);
+    res.status(500).send({ message: "User not created", success: false });
+  }
 };
 
 module.exports = {
   Login,
   Register,
+  Assessment,
+  Summery,
 };
